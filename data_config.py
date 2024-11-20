269668892a16b3b406ac4a20ec226af194d9a99d
JovinB <jovin.bains007@gmail.com>
2024-11-19 21:18:28 -0500

    Created a function 'get_data' that can output data that contains rows of matchups during a certain year range

--- data_config.py
+++ data_config.py
@@ -1,11 +1,62 @@
 import pandas as pd
+import random
 
 
 TEAM_DIRECTORY = {}
 
-# create directory of key/value pairs with TeamID/TeamName
-team_data = pd.read_csv("data/MTeams.csv")
-for row in team_data.iterrows():
-    row = row[1] # iterrows() returns a (index, Series) pair where the 'Series' contains the data
-    TEAM_DIRECTORY[row['TeamID']] = row['TeamName']
+def establish_team_directory():
+    # create directory of key/value pairs with TeamID/TeamName
+    team_data = pd.read_csv("data/MTeams.csv")
+    for row in team_data.iterrows():
+        row = row[1] # iterrows() returns a (index, Series) pair where the 'Series' contains the data
+        TEAM_DIRECTORY[row['TeamID']] = row['TeamName']
 
+
+def get_data(start_year, end_year, conf_label=False):
+    seed_data = pd.read_csv("data/MNCAATourneySeeds.csv")
+    tourney_data = pd.read_csv("data/MNCAATourneyCompactResults.csv")
+
+    years = []
+    year = start_year
+    while not year == end_year + 1:
+        years.append(year)
+        year += 1
+
+    seed_data_subset = seed_data.loc[seed_data['Season'].isin(years)]
+    tourney_data_subset = tourney_data.loc[tourney_data['Season'].isin(years)]
+
+    data = []
+
+    for game in tourney_data_subset.iterrows():
+        game = game[1] # iterrows() returns a (index, Series) pair where the 'Series' contains the data
+        game_data = []
+
+        w_seed = seed_data_subset.loc[(seed_data_subset['TeamID'] == game['WTeamID']) & (seed_data_subset['Season'] == game['Season'])]['Seed'].values[0]
+        l_seed = seed_data_subset.loc[(seed_data_subset['TeamID'] == game['LTeamID']) & (seed_data_subset['Season'] == game['Season'])]['Seed'].values[0]
+
+        if not conf_label: # if we do not want the conference identifier on the seed
+            w_seed = w_seed.replace('W','')
+            l_seed = l_seed.replace('W','')
+            w_seed = w_seed.replace('X','')
+            l_seed = l_seed.replace('X','')
+            w_seed = w_seed.replace('Y','')
+            l_seed = l_seed.replace('Y','')
+            w_seed = w_seed.replace('Z','')
+            l_seed = l_seed.replace('Z','')
+            w_seed = w_seed.replace('a','')
+            l_seed = l_seed.replace('a','')
+            w_seed = w_seed.replace('b','')
+            l_seed = l_seed.replace('b','')
+
+        if random.random() < 0.5: # randomly choose to order the data as winner, loser, 1 (label) or loser, winner, 0 (label)
+            game_data.append(w_seed)
+            game_data.append(l_seed)
+            game_data.append(1)
+        else:
+            game_data.append(l_seed)
+            game_data.append(w_seed)
+            game_data.append(0)
+
+        data.append(game_data)
+
+get_data(2021, 2022)
