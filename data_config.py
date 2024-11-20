86705e4cb3730398d27168aae3bf317aef332a4f
JovinB <jovin.bains007@gmail.com>
2024-11-20 14:42:35 -0500

    get_averaged_reg_season_data now works across seasons

--- data_config.py
+++ data_config.py
@@ -15,12 +15,12 @@ def establish_team_directory():
 
 def get_averaged_reg_season_data(data_tldr):
     # TODO: make work across seasons
-    years = [2022]
+    years = [2021, 2022]
     data = pd.read_csv("data/MRegularSeasonDetailedResults.csv")
     data = data.loc[data['Season'].isin(years)]
 
     averaged_data = []
-    team_IDs = []
+    teams = []
 
     # data: WFGM,WFGA,WFGM3,WFGA3,WFTM,WFTA,WOR,WDR,WAst,WTO,WStl,WBlk,WPF (again for the losing team, but with prefix 'L' instead of 'W')
 
@@ -28,52 +28,53 @@ def get_averaged_reg_season_data(data_tldr):
         game = game[1] # iterrows() returns a (index, Series) pair where the 'Series' contains the data
         team_1_ID = game['WTeamID']
         team_2_ID = game['LTeamID']
+        season = game['Season']
 
-        if team_1_ID not in team_IDs:
-            team_IDs.append(team_1_ID)
+        if (team_1_ID, season) not in teams:
+            teams.append((team_1_ID, season))
 
-            averaged_data.append([1,team_1_ID,int(game['WFGM']),int(game['WFGA']),int(game['WFGM3']),int(game['WFGA3']),
+            averaged_data.append([1,team_1_ID,season,int(game['WFGM']),int(game['WFGA']),int(game['WFGM3']),int(game['WFGA3']),
                                   int(game['WFTM']),int(game['WFTA']),int(game['WOR']),int(game['WDR']),int(game['WAst']),
                                   int(game['WTO']),int(game['WStl']),int(game['WBlk']),int(game['WPF'])])
         else:
-            for i, team_ID in enumerate(team_IDs):
-                if team_ID == team_1_ID:
-                    averaged_data[i] = [averaged_data[i][0]+1,averaged_data[i][1],
-                                        averaged_data[i][2]+int(game['WFGM']),averaged_data[i][3]+int(game['WFGA']),
-                                        averaged_data[i][4]+int(game['WFGM3']),averaged_data[i][5]+int(game['WFGA3']),
-                                        averaged_data[i][6]+int(game['WFTM']),averaged_data[i][7]+int(game['WFTA']),
-                                        averaged_data[i][8]+int(game['WOR']),averaged_data[i][9]+int(game['WDR']),
-                                        averaged_data[i][10]+int(game['WAst']),averaged_data[i][11]+int(game['WTO']),
-                                        averaged_data[i][12]+int(game['WStl']),averaged_data[i][13]+int(game['WBlk']),
-                                        averaged_data[i][14]+int(game['WPF'])]
+            for i, team in enumerate(teams):
+                if team[0] == team_1_ID and team[1] == season:
+                    averaged_data[i] = [averaged_data[i][0]+1,averaged_data[i][1],averaged_data[i][2],
+                                        averaged_data[i][3]+int(game['WFGM']),averaged_data[i][4]+int(game['WFGA']),
+                                        averaged_data[i][5]+int(game['WFGM3']),averaged_data[i][6]+int(game['WFGA3']),
+                                        averaged_data[i][7]+int(game['WFTM']),averaged_data[i][8]+int(game['WFTA']),
+                                        averaged_data[i][9]+int(game['WOR']),averaged_data[i][10]+int(game['WDR']),
+                                        averaged_data[i][11]+int(game['WAst']),averaged_data[i][12]+int(game['WTO']),
+                                        averaged_data[i][13]+int(game['WStl']),averaged_data[i][14]+int(game['WBlk']),
+                                        averaged_data[i][15]+int(game['WPF'])]
                     break
         
-        if team_2_ID not in team_IDs:
-            team_IDs.append(team_2_ID)
+        if (team_2_ID, season) not in teams:
+            teams.append((team_2_ID, season))
 
-            averaged_data.append([1,team_2_ID,int(game['LFGM']),int(game['LFGA']),int(game['LFGM3']),int(game['LFGA3']),
+            averaged_data.append([1,team_2_ID,season,int(game['LFGM']),int(game['LFGA']),int(game['LFGM3']),int(game['LFGA3']),
                                   int(game['LFTM']),int(game['LFTA']),int(game['LOR']),int(game['LDR']),int(game['LAst']),
                                   int(game['LTO']),int(game['LStl']),int(game['LBlk']),int(game['LPF'])])
         else:
-            for i, team_ID in enumerate(team_IDs):
-                if team_ID == team_2_ID:
-                    averaged_data[i] = [averaged_data[i][0]+1,averaged_data[i][1],
-                                        averaged_data[i][2]+int(game['LFGM']),averaged_data[i][3]+int(game['LFGA']),
-                                        averaged_data[i][4]+int(game['LFGM3']),averaged_data[i][5]+int(game['LFGA3']),
-                                        averaged_data[i][6]+int(game['LFTM']),averaged_data[i][7]+int(game['LFTA']),
-                                        averaged_data[i][8]+int(game['LOR']),averaged_data[i][9]+int(game['LDR']),
-                                        averaged_data[i][10]+int(game['LAst']),averaged_data[i][11]+int(game['LTO']),
-                                        averaged_data[i][12]+int(game['LStl']),averaged_data[i][13]+int(game['LBlk']),
-                                        averaged_data[i][14]+int(game['LPF'])]
+            for i, team in enumerate(teams):
+                if team[0] == team_2_ID and team[1] == season:
+                    averaged_data[i] = [averaged_data[i][0]+1,averaged_data[i][1],averaged_data[i][2],
+                                        averaged_data[i][3]+int(game['LFGM']),averaged_data[i][4]+int(game['LFGA']),
+                                        averaged_data[i][5]+int(game['LFGM3']),averaged_data[i][6]+int(game['LFGA3']),
+                                        averaged_data[i][7]+int(game['LFTM']),averaged_data[i][8]+int(game['LFTA']),
+                                        averaged_data[i][9]+int(game['LOR']),averaged_data[i][10]+int(game['LDR']),
+                                        averaged_data[i][11]+int(game['LAst']),averaged_data[i][12]+int(game['LTO']),
+                                        averaged_data[i][13]+int(game['LStl']),averaged_data[i][14]+int(game['LBlk']),
+                                        averaged_data[i][15]+int(game['LPF'])]
                     break
 
     for i in range(len(averaged_data)):
         total_games = averaged_data[i][0]
-        averaged_data[i] = [averaged_data[i][1],averaged_data[i][2]/total_games,averaged_data[i][3]/total_games,
+        averaged_data[i] = [averaged_data[i][1],averaged_data[i][2],averaged_data[i][3]/total_games,
                             averaged_data[i][4]/total_games,averaged_data[i][5]/total_games,averaged_data[i][6]/total_games,
                             averaged_data[i][7]/total_games,averaged_data[i][8]/total_games,averaged_data[i][9]/total_games,
                             averaged_data[i][10]/total_games,averaged_data[i][11]/total_games,averaged_data[i][12]/total_games,
-                            averaged_data[i][13]/total_games,averaged_data[i][14]/total_games]
+                            averaged_data[i][13]/total_games,averaged_data[i][14]/total_games,averaged_data[i][15]/total_games]
 
     return averaged_data
 
@@ -127,10 +128,3 @@ def get_data(start_year, end_year):
         data.append(game_data)
     
     return np.array(data)
-
-testing_data = get_averaged_reg_season_data([1])
-
-for row in testing_data:
-    if row[0] == 1101:
-        print(row)
-        break
