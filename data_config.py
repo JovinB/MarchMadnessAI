7f06e473c8aac0b0f59651f35fbb2ad52f01d78d
JovinB <jovin.bains007@gmail.com>
2024-11-19 21:50:16 -0500

    Have set up a simple model that can achieve 70% accuracy using only the seeds of the teams playing

--- data_config.py
+++ data_config.py
@@ -1,18 +1,20 @@
 import pandas as pd
 import random
-
-
-TEAM_DIRECTORY = {}
+import numpy as np
 
 def establish_team_directory():
+    team_directory = {}
+
     # create directory of key/value pairs with TeamID/TeamName
     team_data = pd.read_csv("data/MTeams.csv")
     for row in team_data.iterrows():
         row = row[1] # iterrows() returns a (index, Series) pair where the 'Series' contains the data
-        TEAM_DIRECTORY[row['TeamID']] = row['TeamName']
+        team_directory[row['TeamID']] = row['TeamName']
+
+    return team_directory
 
 
-def get_data(start_year, end_year, conf_label=False):
+def get_data(start_year, end_year):
     seed_data = pd.read_csv("data/MNCAATourneySeeds.csv")
     tourney_data = pd.read_csv("data/MNCAATourneyCompactResults.csv")
 
@@ -34,29 +36,31 @@ def get_data(start_year, end_year, conf_label=False):
         w_seed = seed_data_subset.loc[(seed_data_subset['TeamID'] == game['WTeamID']) & (seed_data_subset['Season'] == game['Season'])]['Seed'].values[0]
         l_seed = seed_data_subset.loc[(seed_data_subset['TeamID'] == game['LTeamID']) & (seed_data_subset['Season'] == game['Season'])]['Seed'].values[0]
 
-        if not conf_label: # if we do not want the conference identifier on the seed
-            w_seed = w_seed.replace('W','')
-            l_seed = l_seed.replace('W','')
-            w_seed = w_seed.replace('X','')
-            l_seed = l_seed.replace('X','')
-            w_seed = w_seed.replace('Y','')
-            l_seed = l_seed.replace('Y','')
-            w_seed = w_seed.replace('Z','')
-            l_seed = l_seed.replace('Z','')
-            w_seed = w_seed.replace('a','')
-            l_seed = l_seed.replace('a','')
-            w_seed = w_seed.replace('b','')
-            l_seed = l_seed.replace('b','')
+        # we do not want the conference identifier on the seed
+        w_seed = w_seed.replace('W','')
+        l_seed = l_seed.replace('W','')
+        w_seed = w_seed.replace('X','')
+        l_seed = l_seed.replace('X','')
+        w_seed = w_seed.replace('Y','')
+        l_seed = l_seed.replace('Y','')
+        w_seed = w_seed.replace('Z','')
+        l_seed = l_seed.replace('Z','')
+        w_seed = w_seed.replace('a','')
+        l_seed = l_seed.replace('a','')
+        w_seed = w_seed.replace('b','')
+        l_seed = l_seed.replace('b','')
 
         if random.random() < 0.5: # randomly choose to order the data as winner, loser, 1 (label) or loser, winner, 0 (label)
-            game_data.append(w_seed)
-            game_data.append(l_seed)
+            game_data.append(int(w_seed))
+            game_data.append(int(l_seed))
             game_data.append(1)
         else:
-            game_data.append(l_seed)
-            game_data.append(w_seed)
+            game_data.append(int(l_seed))
+            game_data.append(int(w_seed))
             game_data.append(0)
 
         data.append(game_data)
+    
+    return np.array(data)
 
 get_data(2021, 2022)
---
+++ test_models.py
@@ -0,0 +1,21 @@
+import data_config
+import NN_models
+
+def test():
+    TEAM_DIRECTORY = data_config.establish_team_directory()
+
+    data = data_config.get_data(1985, 2022)
+    print(data)
+    data_X = data[:,:-1]
+    data_Y = data[:,-1]
+    
+    num_features = len(data_X[0])
+
+    NN_1 = NN_models.simple_MLP(num_features)
+
+    NN_1.fit(data_X, data_Y, epochs=20, batch_size=32) #, validation_split=0.2)
+    test_loss, test_accuracy = NN_1.evaluate(data_X, data_Y)
+    print(f"Test loss: {test_loss:.2f}")
+    print(f"Test accuracy: {test_accuracy:.2f}")
+
+test()
