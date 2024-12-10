d37115bb3647cfbbb8fa3ef95f32f62419068238
JovinB <jovin.bains007@gmail.com>
2024-12-10 18:15:16 -0500

    Testing is setup for different models and different datasets

--- test_models.py
+++ test_models.py
@@ -25,9 +25,9 @@ def test():
 
     #baseline_acc = baseline_by_seed(data_X, data_y)
     
-    data = data_config.get_data(2022, 2022, dataset="Regular")
-    kenpom_data = data_config.get_data(2022, 2022, dataset="KenPom")
-    both_data = data_config.get_data(2022, 2022, dataset="Both")
+    data = data_config.get_data(2015, 2022, dataset="Regular")
+    kenpom_data = data_config.get_data(2015, 2022, dataset="KenPom")
+    both_data = data_config.get_data(2015, 2022, dataset="Both")
 
     np.random.shuffle(data)
     np.random.shuffle(kenpom_data)
@@ -45,31 +45,41 @@ def test():
     KP_test_data_X = KP_test_data[:,:-1]
     KP_test_data_y = KP_test_data[:,-1]
 
-    combined_train_data, combined_test_data = train_test_split(data, test_size=0.1)
-    combined_train_data_X = combined_train_data[:,:-1]
-    combined_train_data_y = combined_train_data[:,-1]
-    combined_test_data_X = combined_test_data[:,:-1]
-    combined_test_data_y = combined_test_data[:,-1]
+    CB_train_data, CB_test_data = train_test_split(data, test_size=0.1)
+    CB_train_data_X = CB_train_data[:,:-1]
+    CB_train_data_y = CB_train_data[:,-1]
+    CB_test_data_X = CB_test_data[:,:-1]
+    CB_test_data_y = CB_test_data[:,-1]
     
     num_features = len(train_data_X[0])
     KP_num_features = len(KP_train_data_X[0])
-    combined_num_features = len(combined_train_data_X[0])
+    CB_num_features = len(CB_train_data_X[0])
 
     # NN_1 = NN_models.very_simple_MLP(num_features)
     NN_2 = NN_models.simple_MLP(num_features)
     KP_NN_2 = NN_models.simple_MLP(KP_num_features)
-    CB_NN_2 = NN_models.simple_MLP(combined_num_features)
+    CB_NN_2 = NN_models.simple_MLP(CB_num_features)
     
-    NN_2.fit(train_data_X, train_data_y, epochs=100, batch_size=32) #, validation_split=0.2)
-    KP_NN_2.fit(KP_train_data_X, KP_train_data_y, epochs=100, batch_size=32)
-    CB_NN_2.fit(combined_train_data_X, combined_train_data_y, epochs=100, batch_size=32)
+    history = NN_2.fit(train_data_X, train_data_y, epochs=1000, batch_size=32, validation_data=(test_data_X, test_data_y))
+    KP_history = KP_NN_2.fit(KP_train_data_X, KP_train_data_y, epochs=1000, batch_size=32, validation_data=(KP_test_data_X, KP_test_data_y))
+    CB_history = CB_NN_2.fit(CB_train_data_X, CB_train_data_y, epochs=1000, batch_size=32, validation_data=(CB_test_data_X, CB_test_data_y))
+    
+    accuracies = history.history['val_accuracy']
+    KP_accuracies = KP_history.history['val_accuracy']
+    CB_accuracies = CB_history.history['val_accuracy']
+
+    losses = history.history['val_loss']
+    KP_losses = KP_history.history['val_loss']
+    CB_losses = CB_history.history['val_loss']
+    
+
+    print(f"Test accuracy: {max(accuracies):.2f}, Test loss: {min(losses):.2f}")
+    print(f"KP Test accuracy: {max(KP_accuracies):.2f}, KP Test loss: {min(KP_losses):.2f}")
+    print(f"CB Test accuracy: {max(CB_accuracies):.2f}, CB Test loss: {min(CB_losses):.2f}")
+    
+    # print(f"Baseline accuracy: {baseline_acc:.2f}")
 
     # TODO: early stopping
     # TODO: normalize features?
-
-    # test_loss, test_accuracy = NN_2.evaluate(test_data_X, test_data_y)
-    # print(f"Test loss: {test_loss:.2f}")
-    # print(f"Test accuracy: {test_accuracy:.2f}")
-    #print(f"Baseline accuracy: {baseline_acc:.2f}")
 
 test()
